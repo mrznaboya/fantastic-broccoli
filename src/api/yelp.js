d7ab916de55c920f08a633babcc62012c95bf10b
mrznaboya <zuyax@yahoo.com>
2023-11-11 12:17:19 +0800

    re-up

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorization: "Bearer <Your API key>",
+    Authorization:
+      "Bearer y_KfXNxQwefRIxEAyPaLZZ-aIJXAPxmJRIXgMUvmr4y1fIwD6AyLQjy-S8BE84e9wxq9ae0BkokPCSk_qJP-Z1eQ-xuw1CjU-dW-ULdL5rFhGjoKiN_JdYEreGk-ZXYx",
   },
 });
